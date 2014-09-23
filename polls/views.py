8e97f96c55c0e5ca3973e43cf4514623250bb225
rightgo09 <skyarrow09@gmail.com>
2014-09-23 14:47:39 +0900

    using shortcut method 'render'

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
@@ -6,11 +6,8 @@ from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
